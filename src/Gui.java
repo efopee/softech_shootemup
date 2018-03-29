dc208881b031cf3f1280675dc44a74da625f13e2
bdani <bdani1996@gmail.com>
2018-03-29 15:43:46 +0200

    draw image tried

--- src/Gui.java
+++ src/Gui.java
@@ -1,11 +1,16 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+import java.io.File;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -105,12 +110,32 @@ public class Gui extends JFrame {
 		
 		private ArrayList<Point> bullets = new ArrayList<Point>();
 		private ArrayList<Point> players = new ArrayList<Point>();
+		private ArrayList<Point> enemyies = new ArrayList<Point>();
+		
+		
 		
 
 
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			
+		    try {
+		        Graphics2D g2D;
+		        g2D = (Graphics2D) g;
+		        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		        String fileName = "enemy1.PNG";
+		        Image img = getToolkit().getImage(fileName);
+		        Image smallImage = img.getScaledInstance(30, -1, Image.SCALE_DEFAULT);
+		        AffineTransform aTran = new AffineTransform();
+		        aTran.translate(50.0f, 20.0f);
+		        g2D.transform(aTran);
+		        g2D.drawImage(img, 30, 30, 50, 50, this);
+		      }
+		    
+		    catch (Exception e) {
+		    	  System.out.println("error during loading image");
+		      }
 			
 			g.setColor(Color.green);
 			
@@ -123,10 +148,15 @@ public class Gui extends JFrame {
 			
 			for (Point player : players){
 				g.fillRect(player.x, player.y, 50, 5);
+				
 			}
+			
+			
 
 		}
 	}
+	
+	
 	
 	
 
